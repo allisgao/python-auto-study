6316cdf48c07c90864be33f63b718b6ecf8afff5
allisgao <gaojz017@163.com>
2017-08-02 18:51:12 +0800

    practices in a book named Automate The Boring Stuff With Python.

---
+++ Section1_basic/Part2/2.9_exitExample.py
@@ -0,0 +1,9 @@
+#coding = utf-8
+import sys
+while True:
+    print('Type exit to quit.')
+    response = input()
+    if response == 'exit':
+        sys.exit(1)
+    #print('You typed '+ response + '.')
+#print('You typed '+ response + '.')
