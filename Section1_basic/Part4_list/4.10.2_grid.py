a8300f3c616a8a07e443959946c97e44188bd209
allisgao <gaojz017@163.com>
2017-08-09 17:13:40 +0800

    practice 4.10.1 has been completely.

---
+++ Section1_basic/Part4_list/4.10.2_grid.py
@@ -0,0 +1,26 @@
+#coding = utf-8
+def trans(mylist):
+    for i in range(0, int(len(mylist[0]))):
+        print('')
+        for j in range(0,int(len(mylist))):
+            print(mylist[j][i],end='')
+
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+trans(grid)
+
+
+
+
+
+
+
