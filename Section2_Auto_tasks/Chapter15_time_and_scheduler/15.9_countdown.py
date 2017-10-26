c17e3293d533f020af295eaad0659298f79920d0
allisgao <gaojz017@163.com>
2017-10-26 11:18:17 +0800

    简单修改，添加注释

--- Section2_Auto_tasks/Chapter15_time_and_scheduler/15.9_countdown.py
+++ Section2_Auto_tasks/Chapter15_time_and_scheduler/15.9_countdown.py
@@ -5,7 +5,8 @@ import time, subprocess
 
 timeLeft = 60
 while timeLeft > 0:
-    # print(timeLeft, end=' ')
+    # print(timeLeft, end='')
+    # 如果加上后面的“end=''”，程序会在结束后一股脑把倒计时数字输出
     print(timeLeft)
     time.sleep(1)
     timeLeft = timeLeft - 1
