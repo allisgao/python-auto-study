83de8a2bf4006f0ab29f3b814179d0e034cb2036
allisgao <gaojz017@163.com>
2017-10-26 11:16:14 +0800

    多线程从xkcd下载图片

---
+++ Section2_Auto_tasks/Chapter15_time_and_scheduler/15.7_multidownloadXkcd.py
@@ -0,0 +1,50 @@
+#! python3
+# multidownloadXkcd.py - Download XKCD commics using multiple threads.
+
+import requests, os, bs4, threading
+os.makedirs('xkcd', exist_ok=True)  # sotre comics in ./xkcd
+
+def downloadXkcd(startComic, endComic):
+    for urlNum in range(startComic, endComic):
+        # Download the page.
+        print('Downloading page http://xkcd.com/%s...' %(urlNum))
+        res = requests.get('http://xkcd.com/%s' % (urlNum))
+        res.raise_for_status()
+
+        soup = bs4.BeautifulSoup(res.text)
+
+        # Find the URL of the commic image.
+        comicElem = soup.select('#comic img')
+        if comicElem == []:
+            print('Could not find comic image.')
+        else:
+            comicUrl = comicElem[0].get('src')
+            # Download the image.
+            print('Downloading image %s...' % (comicUrl))
+            res = requests.get(comicUrl)
+            res.raise_for_status()
+
+            # Save the iamge to ./xkcd
+            imageFiel = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+            for chunk in res.iter_content(100000):
+                imageFiel.write(chunk)
+            imageFiel.close()
+
+# Create and start the Thread objects.
+downloadThreads = []
+for i in range(0, 1400, 100):
+    downloadThread = threading.Thread(target=downloadXkcd, args=(i, i+99))
+    downloadThreads.append(downloadThread)
+    downloadThread.start()
+# Wait for all threads to end.
+for downloadThread in downloadThreads:
+    downloadThread.join()
+print('Down.')
+
+
+
+
+
+
+
+
